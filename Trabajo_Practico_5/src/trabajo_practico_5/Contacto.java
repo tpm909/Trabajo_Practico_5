9d44f7eba528be447fb79c6a8d51e7a8766ecead
Tomas-Suarez <tomasa.suarez81@gmail.com>
2024-09-03 17:18:15 -0300

    implementacion del JFrame y configuracion de algunos botones

---
+++ Trabajo_Practico_5/src/trabajo_practico_5/Contacto.java
@@ -0,0 +1,78 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package trabajo_practico_5;
+
+/**
+ *
+ * @author Tomi
+ */
+public class Contacto {
+    
+    private int dni;
+    private String nombre;
+    private String apellido;
+    private String direccion;
+    private String ciudad;
+    
+    //constructor
+
+    public Contacto() {}
+    
+
+    public Contacto(int dni, String nombre, String apellido, String direccion, String ciudad) {
+        this.dni = dni;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.direccion = direccion;
+        this.ciudad = ciudad;
+    }
+    
+    public int getDni() {
+        return dni;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    @Override
+    public String toString() {
+        return "Contacto{" + "dni=" + dni + ", nombre=" + nombre + ", apellido=" + apellido + ", direccion=" + direccion + ", ciudad=" + ciudad + '}';
+    }
+    
+    
+}
---
+++ Trabajo_Practico_5/src/trabajo_practico_5/Directorio.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package trabajo_practico_5;
+
+import java.util.TreeMap;
+import java.util.TreeSet;
+
+/**
+ *
+ * @author Tomi
+ */
+public class Directorio {
+    
+   private TreeMap<Long, Contacto> contactos = new TreeMap();
+    
+   private TreeSet<Contacto> cosaInutil =new TreeSet();
+
+   public Directorio() {
+   }
+   
+   public void agregarContacto (Long telefono, Contacto otroContacto){
+       contactos.put(telefono, otroContacto);
+   }
+   
+   public void buscarContacto (){
+       
+   }
+   public void buscarTelefono (){
+       
+   }
+   public void buscarContactos (){
+       
+   }
+   public void borrarContacto (Long telefono){
+       contactos.remove(telefono);
+   }
+}
